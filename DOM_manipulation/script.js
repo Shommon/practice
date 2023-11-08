ff4d4a359f7c57f8b8eeed3416b2fc4ccae86c75
Shommon <sdtai2@outlook.com>
2023-11-08 00:36:12 -0500

    Created DOM_manipulation directory Practiced DOM_manipulation techniques

---
+++ DOM_manipulation/script.js
@@ -0,0 +1,45 @@
+const container = document.querySelector('#container');
+
+const content = document.createElement('div'); //Creates a div element
+content.classList.add('content'); // Adds class "content" to content
+content.textContent='Hello World!'
+
+
+const par = document.createElement('p');
+par.textContent = "Hey I'm red!";
+par.style.color = 'red';
+
+const head3 = document.createElement('h3');
+head3.textContent = "I'm a blue h3!"
+head3.style.cssText = 'color: blue'
+
+const contentBorder = document.createElement('div');
+contentBorder.classList.add('border')
+contentBorder.style.cssText = 'border: 2px solid black; background-color: pink'
+
+const head1 = document.createElement('h1');
+head1.textContent ="I'm in a div!"
+
+const par2 = document.createElement('p');
+par2.textContent = 'ME TOO!'
+
+
+const btn = document.querySelector('#btn');
+btn.onclick = () => alert('Hello World!');
+
+function alertFunction() {
+    alert("YEAAAAH BABBBYY!")
+}
+
+const btn2 = document.querySelector('#btn2');
+btn2.addEventListener('click', alertFunction);
+
+// Children of Content Border
+contentBorder.appendChild(head1);
+contentBorder.appendChild(par2)
+// Children of Container
+container.appendChild(par);
+container.appendChild(head3);
+container.appendChild(content);
+container.appendChild(contentBorder);
+
